07c4c7c28c162cb05a6c3e96b55b4358eb812d92
NiallRees <niallrwoodward@gmail.com>
2020-06-13 13:47:49 +0100

    make the map functional

--- src/components/map.js
+++ src/components/map.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactMapGL, {Marker, FlyToInterpolator, NavigationControl, ScaleControl} from "react-map-gl";
 import * as d3 from 'd3-ease';
 import config from '../config';
@@ -13,41 +13,44 @@ const TOKEN=config.MAPBOX_TOKEN;
 const GEOCODEURL='https://api.mapbox.com/geocoding/v5/mapbox.places/'
 const MAPSEARCHINTERVAL=1500
 
-class Map extends Component {
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
 
-  constructor(props) {
-    super(props);
-    const lngLat = [this.props.viewport.longitude, this.props.viewport.latitude];
-    this.props.updateMouseCoords(lngLat);
-    this.searchForLocation = this.searchForLocation.bind(this);
-  }
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.searchForLocation()
-    }, MAPSEARCHINTERVAL)
-  }
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
-  calculateLatLong(coords) {
-    let lats = coords.map(a => a.latitude);
-    let longs = coords.map(a => a.longitude);
-    let middle_lat = (Math.min(...lats) + Math.max(...lats))/2;
-    let middle_long = (Math.min(...longs) + Math.max(...longs))/2;
-    return [middle_lat, middle_long];
-  }
+function Map(props) {
 
-  renderMarkers() {
-    if (this.props.markers) {
+  useInterval(() => {
+    searchForLocation()
+  }, MAPSEARCHINTERVAL);
+
+  const renderMarkers = () => {
+    if (props.markers) {
       return(
-        this.props.markers.map(marker => 
-          this.renderMarker({marker}),
+        props.markers.map(marker => 
+          renderMarker(marker),
         )
       )
     }
   }
 
-  renderMarker({ marker }) {
-    const selectedMarkerID = this.props.selectedMarker ? this.props.selectedMarker.id : null
+  const renderMarker = (marker) => {
+    const selectedMarkerID = props.selectedMarker ? props.selectedMarker.id : null
     const selected = (marker.id === selectedMarkerID);
     if (marker.type === "sensor") {
       return (
@@ -55,8 +58,8 @@ class Map extends Component {
           <SensorPin
             sensor={marker}
             selected={selected}
-            clickHandler={this.props.markerClickHandler}
-            mode = {this.props.mode}
+            clickHandler={props.markerClickHandler}
+            mode = {props.mode}
           />
         </Marker>
       )
@@ -67,81 +70,81 @@ class Map extends Component {
           <RechargePin
             sensor={marker}
             selected={selected}
-            clickHandler={this.props.markerClickHandler}
-            mode = {this.props.mode}
+            clickHandler={props.markerClickHandler}
+            mode = {props.mode}
           />
         </Marker>
       )
     }
   }
 
-  renderTakeoffPin() {
-  const takeoff = this.props.takeoff;
+  const renderTakeoffPin = () => {
+  const takeoff = props.takeoff;
   if (takeoff) {
-    const selectedMarkerID = this.props.selectedMarker ? this.props.selectedMarker.id : null
+    const selectedMarkerID = props.selectedMarker ? props.selectedMarker.id : null
     const selected = (takeoff.id === selectedMarkerID);
       return (
         <Marker key={takeoff.id} latitude={takeoff.latitude} longitude={takeoff.longitude}>
           <TakeoffPin
             takeoff={takeoff}
             selected={selected}
-            clickHandler={this.props.markerClickHandler}
-            mode = {this.props.mode}
+            clickHandler={props.markerClickHandler}
+            mode = {props.mode}
           />
         </Marker>
       )
     }
   }
 
-  renderPinPrompt() {
-    if (this.props.pinPrompt.enabled) {
+  const renderPinPrompt = () => {
+    if (props.pinPrompt.enabled) {
       return (
-        <Marker key={99} latitude={this.props.pinPrompt.latitude} longitude={this.props.pinPrompt.longitude}>
-          <PinPrompt PinPrompt={this.props.pinPrompt} pinPromptClickHandler={this.props.pinPromptClickHandler}/>
+        <Marker key={99} latitude={props.pinPrompt.latitude} longitude={props.pinPrompt.longitude}>
+          <PinPrompt PinPrompt={props.pinPrompt} pinPromptClickHandler={props.pinPromptClickHandler}/>
         </Marker>
       )
     }
   }
 
-  planPath() {
-    if (this.props.mode === 'plan' && this.props.planRouteMarkers && this.props.takeoff && this.props.planRouteMarkers.length > 0) {
-      var lineCoords = this.props.planRouteMarkers.map(sensor =>
+  const planPath = () => {
+    if (props.mode === 'plan' && props.planRouteMarkers && props.takeoff && props.planRouteMarkers.length > 0) {
+      var lineCoords = props.planRouteMarkers.map(sensor =>
         [sensor['longitude'], sensor['latitude']]
       )
-      lineCoords.unshift([this.props.takeoff['longitude'], this.props.takeoff['latitude']])
+      lineCoords.unshift([props.takeoff['longitude'], props.takeoff['latitude']])
       return (
         <PolylineOverlay points={lineCoords} />
       )
     }
   }
 
-  searchForLocation() {
-    const searchLocation = this.props.searchLocation;
+  const searchForLocation = () => {
+    const searchLocation = props.searchLocation;
     if(searchLocation === '') { // Removes the results when search box is cleared
-      this.props.setSearchResults([])
+      props.setSearchResults([])
     }
-    if ((searchLocation !== this.props.lastSearchLocation) && searchLocation !== '') {
+    if ((searchLocation !== props.lastSearchLocation) && searchLocation !== '') {
 
       fetch(GEOCODEURL.concat(searchLocation, '.json?access_token=', TOKEN))
       .then(res => res.json())
       .then((data) => {
-        this.props.setSearchResults(data.features)
+        props.setSearchResults(data.features)
       })
       .catch(console.log)
-      this.props.setLastSearchLocation(searchLocation)
+      props.setLastSearchLocation(searchLocation)
     }
   }
 
-  searchResultClickHandler(result) {
+  const searchResultClickHandler = (result) => {
     if (result.bbox) {
-      var {longitude, latitude, zoom} = this.props.centeredViewport(result.bbox[1], result.bbox[0], result.bbox[3], result.bbox[2])
+      var {longitude, latitude, zoom} = props.centeredViewport(result.bbox[1], result.bbox[0], result.bbox[3], result.bbox[2])
     } else { // In case the result doesn't have a bbox eg a street
       var longitude = result.center[0]
       var latitude = result.center[1]
       var zoom = 16
     }
     const viewport = {
-      ...this.props.viewport,
+      ...props.viewport,
       longitude,
       latitude,
       zoom: zoom+1,
@@ -149,21 +152,21 @@ class Map extends Component {
       transitionInterpolator: new FlyToInterpolator(),
       transitionEasing: d3.easeCubic
     }
-    this.props.setViewport(viewport)
-    this.props.setShowSearchResults(false)
+    props.setViewport(viewport)
+    props.setShowSearchResults(false)
   }
 
-  renderSearchResults() {
-    if(this.props.showSearchResults) {
+  const renderSearchResults = () => {
+    if(props.showSearchResults) {
       return (
         <div id="search-results">
           <ol id="search-results-list">
-            {this.props.searchResults.map(result =>
+            {props.searchResults.map(result =>
               <li
                 id="search-result-item"
                 key={result.id}
                 onClick={() => {
-                  this.searchResultClickHandler(result)
+                  searchResultClickHandler(result)
                 }}
               >
                 {result.place_name}
@@ -175,66 +178,64 @@ class Map extends Component {
     }
   }
 
-  renderSearchBox() {
+  const renderSearchBox = () => {
     return(
       <div id="search-box">
         <input
           id="search-input"
           placeholder="Search"
           onInput={(e) => {
-            this.props.setSearchLocation(e.target.value)
+            props.setSearchLocation(e.target.value)
           }}
           onFocus={(e) => {
-            this.props.setShowSearchResults(true)
+            props.setShowSearchResults(true)
           }}
         >
         </input>
-        {this.renderSearchResults()}
+        {renderSearchResults()}
       </div>
     )
   }
 
-  render() {
-    return (
-      <>
-        <ReactMapGL
-          mapStyle="mapbox://styles/mapbox/streets-v11"
-          {...this.props.viewport}
-          mapboxApiAccessToken={TOKEN}
-          width='100%'
-          height='100%'
-          onViewportChange={viewport => this.props.onViewportChange(viewport)}
-          onContextMenu={(e) => {
-            this.props.enablePinPrompt(e.lngLat);
-            e.preventDefault();
-          }}
-          onClick={(e) => {
-            this.props.mapClickHandler(e);
-            this.props.setShowSearchResults(false)
-          }}
-          onMouseMove={(e) => {
-            this.props.updateMouseCoords(e.lngLat)
-          }}
-        >
-          <div id="navigation">
-            <NavigationControl />
-          </div>
-          <div id="scale">
-            <ScaleControl maxWidth={200} unit={"metric"}/>
-          </div>
-          {this.planPath()}
-          {this.renderMarkers()}
-          {this.renderTakeoffPin()}
-          {this.renderPinPrompt()}
-          <div className="coords-box">
-            <pre className="coord">Latitude: {this.props.mouseCoords.latitude}</pre>
-            <pre className="coord">Longitude: {this.props.mouseCoords.longitude}</pre>
-          </div>
-        </ReactMapGL>
-        {this.renderSearchBox()}
-      </>
-    );
-  }
+  return (
+    <>
+      <ReactMapGL
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        {...props.viewport}
+        mapboxApiAccessToken={TOKEN}
+        width='100%'
+        height='100%'
+        onViewportChange={viewport => props.onViewportChange(viewport)}
+        onContextMenu={(e) => {
+          props.enablePinPrompt(e.lngLat);
+          e.preventDefault();
+        }}
+        onClick={(e) => {
+          props.mapClickHandler(e);
+          props.setShowSearchResults(false)
+        }}
+        onMouseMove={(e) => {
+          props.updateMouseCoords(e.lngLat)
+        }}
+      >
+        <div id="navigation">
+          <NavigationControl />
+        </div>
+        <div id="scale">
+          <ScaleControl maxWidth={200} unit={"metric"}/>
+        </div>
+        {planPath()}
+        {renderMarkers()}
+        {renderTakeoffPin()}
+        {renderPinPrompt()}
+        <div className="coords-box">
+          <pre className="coord">Latitude: {props.mouseCoords.latitude}</pre>
+          <pre className="coord">Longitude: {props.mouseCoords.longitude}</pre>
+        </div>
+      </ReactMapGL>
+      {renderSearchBox()}
+    </>
+  );
 }
 
 export default Map;
